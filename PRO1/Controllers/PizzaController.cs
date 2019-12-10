3a4c68f3cd25e9f2f5dbf5d61ad29f583d030027
LuczajK <48481555+LuczajK@users.noreply.github.com>
2019-12-11 00:23:30 +0100

    wq ZZZZ

---
+++ PRO1/Controllers/PizzaController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PRO1.Models;
+
+namespace PRO1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzaController : ControllerBase
+    {
+        private s18185Context _context;
+        public PizzaController(s18185Context context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult getPizza()
+        {
+            return Ok(_context.pizza);
+        }
+    }
+}
